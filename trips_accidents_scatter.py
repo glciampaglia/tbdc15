b4a1633977bc287ec09972ee9e6ee2e12bc23146
Giovanni Luca Ciampaglia <gciampag@indiana.edu>
2015-11-02 00:05:44 -0500

    Added notebook for computing accidents / trips and storing everything into HDF format

--- trips_accidents_scatter.py
+++ trips_accidents_scatter.py
@@ -1,19 +1,59 @@
 from pylab import *
+import os
 import pandas
+import tarfile
 scale = 1000.0
-df = pandas.read_csv('/home/giovanni/data/telecom_big_data_challenge_2015/output_20150301_PALERMO.txt.gz', compression='gzip', names=['trip_id','timestamp','lat','lon','type','speed'], sep=';')
-df_box = pandas.read_csv('/home/giovanni/data/telecom_big_data_challenge_2015/city_boxes.csv', index_col='city')
-df_box.ix['p']
-box = df_box.ix['p'].to_dict()
-df['i'] = ((df['lat'] - box['lat_min']) * scale).round()
-df['j'] = ((df['lon'] - box['lon_min']) * scale).round()
-trips = df.groupby(['i', 'j'], as_index=False).count().filter(['i','j','trip_id']).rename(columns={'trip_id': 'trips'})
-accidents = pandas.read_csv('/home/giovanni/data/telecom_big_data_challenge_2015/BDC2015_UnipolsaiClaims2014_PA.csv')
+
+trips_path = os.path.expanduser('~/data/tbdc2015/infoblu/palermo.tar.gz')
+accidents_path = os.path.expanduser('~/data/tbdc2015/unipol/BDC2015_UnipolsaiClaims2014_PA.csv')
+boxes_path = os.path.expanduser('~/data/tbdc2015/city_boxes.csv')
+city_code = 'p'
+
+# ## Read data
+
+# ### Bounding boxes data
+df_box = pandas.read_csv(boxes_path, index_col='city')
+df_box.ix[city_code]
+box = df_box.ix[city_code].to_dict()
+
+# ### Trips data
+index_columns = ['i', 'j', 'weekday', 'hour']
+with tarfile.open(trips_path, mode='r:gz') as tf:
+    # open tar file in random access mode with on-the-fly gzip decompression
+    frames = []
+    for member in tf:
+        print member.name
+        f = tf.extractfile(member)
+        df = pandas.read_csv(f, names=['trip_id','timestamp','lat','lon','type','speed'], sep=';', parse_dates=['timestamp'])
+        df.rename(columns={'trip_id': 'trips'}, inplace=True)
+        df['i'] = ((df['lat'] - box['lat_min']) * scale).round()
+        df['j'] = ((df['lon'] - box['lon_min']) * scale).round()
+        df['weekday'] = df['timestamp'].map(pandas.Timestamp.weekday)
+        df['hour'] = df['timestamp'].map(lambda k: k.hour)
+        frames.append(df)
+
+df = pandas.concat(frames)
+trips = df.groupby(['i', 'j', 'weekday', 'hour'], as_index=False).count().filter(index_columns + ['trips'])
+
+# ### Accidents data
+accidents = pandas.read_csv(accidents_path)
+accidents.rename(columns={'day_type': 'weekday', 'time_range': 'hour'}, inplace=True)
 accidents['i'] = ((accidents['latitude'] - box['lat_min']) * scale).round()
 accidents['j'] = ((accidents['longitude'] - box['lon_min']) * scale).round()
-accidents_grouped = accidents.groupby(['i', 'j'], as_index=False).count().filter(['i','j','day_type']).rename(columns={'day_type': 'accidents'})
-joined_df = trips.set_index(['i','j']).join(accidents_grouped.set_index(['i','j'])).fillna(0)
+accidents['accidents'] = 1
+accidents_grouped = accidents.groupby(['i', 'j', 'weekday', 'hour'], as_index=False).count().filter(index_columns + ['accidents'])
+
+# ## Join data
+joined_df = trips.set_index(index_columns).join(accidents_grouped.set_index(index_columns)).fillna(0)
+joined_df.to_hdf("trip_accidents_store.hdf", 'palermo')
+print "Data saved to HDF."
+
+# ## Plot
+#
+# ### Scatter plot of each cell
 joined_df.plot(x='trips', y='accidents', linestyle='none', marker='x', alpha=.2, color='k')
+#
+# ### Bin by traffic density level and average
 joined_df.groupby(joined_df.trips // 10).mean().plot(x='trips', y='accidents',
                                                      color='r', linestyle='solid',
                                                      ax=gca(), alpha=.5, linewidth=2)
